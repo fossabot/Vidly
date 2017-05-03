929747bb6c4379c52b58bb73227961a0986a8d04
rogermoka <roger.mokarzel@gmail.com>
2017-05-02 23:30:09 -0300

    Exercise 85 - DataTable  reading from movies API

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -24,8 +24,9 @@ namespace Vidly.Controllers
         }
         public ViewResult Index()
         {
-         var movies = _context.Movies.Include(m => m.Genre).ToList();
-         return View(movies);
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+
+            return View(movies);
         }
 
         public ActionResult New()
